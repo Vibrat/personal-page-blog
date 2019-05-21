e8b3ab5fc720a43446151a6baf972741109bcdd9
DESKTOP-G47CM0M\admin <lam.nguyen@onsolve.com>
2019-05-21 13:44:56 +0700

    Add Password Component

--- src/app/admin/components/dashboard/components/password/admin-dashboard-password.component.ts
+++ src/app/admin/components/dashboard/components/password/admin-dashboard-password.component.ts
@@ -1,8 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-    selector: `admin-dashboard-password`,
-    templateUrl: 'admin-dashboard-password.component.html',
-    styleUrls: ['admin-dashboard-password.component.scss']
+  selector: `admin-dashboard-password`,
+  templateUrl: "admin-dashboard-password.component.html",
+  styleUrls: ["admin-dashboard-password.component.scss"]
 })
-export class AdminDashboardPasswordComponent {}
+export class AdminDashboardPasswordComponent implements OnInit {
+  validateForm: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+
+  genderChange(value: string): void {
+    this.validateForm
+      .get("note")!
+      .setValue(value === "male" ? "Hi, man!" : "Hi, lady!");
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      password: [null, [Validators.required]],
+      passwordConfirm: [null, [Validators.required]]
+    });
+  }
+}
