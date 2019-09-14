30063419aef133fd59a6508efe84dc2a7139dd2d
DESKTOP-G47CM0M\admin <lam.nguyen@onsolve.com>
2019-09-14 14:42:19 +0700

    Add comitted value into search button component

--- src/app/admin/components/dashboard/components/search/search.component.ts
+++ src/app/admin/components/dashboard/components/search/search.component.ts
@@ -1,21 +1,24 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-    selector: 'search-comp',
-    templateUrl: 'search.component.html',
-    styleUrls: ['search.component.scss']
+  selector: "search-comp",
+  templateUrl: "search.component.html",
+  styleUrls: ["search.component.scss"]
 })
 export class SearchComponent {
-    
-    searchValue: string;
-    
-    @Input('default') defaultValue = ''; // default value    
-    @Output('onCommit') data: EventEmitter<string> = new EventEmitter();
+  searchValue: string; // binding variable to input
+  currentValue: string; // Value to show below button search
 
-    search() { this.data.emit(this.searchValue); }
-    reset() { 
+  @Input("default") defaultValue = ""; // default value
+  @Output("onCommit") data: EventEmitter<string> = new EventEmitter();
 
-        this.searchValue = this.defaultValue; 
-        this.data.emit(this.defaultValue);
-    }
+  search() {
+    this.data.emit(this.searchValue);
+    this.currentValue = this.searchValue;
+  }
+  reset() {
+    this.searchValue = this.defaultValue;
+    this.currentValue = this.defaultValue;
+    this.data.emit(this.defaultValue);
+  }
 }
