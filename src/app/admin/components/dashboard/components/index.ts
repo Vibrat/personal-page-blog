57c4808ca0e94b15617dc8e94e69ac96ff08d4c1
DESKTOP-G47CM0M\admin <lam.nguyen@onsolve.com>
2019-09-14 17:21:06 +0700

    Add UI Content for New Group Section

--- src/app/admin/components/dashboard/components/index.ts
+++ src/app/admin/components/dashboard/components/index.ts
@@ -1,12 +1,15 @@
 import { AdminDashboardPasswordComponent } from "./password/dashboard-password.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
+import { NewGroupComponent } from "./new-group/new-group.component";
 import { GroupTagDirective } from "./group-tag/group-tag.directive";
 import { GroupTagComponent } from "./group-tag/group-tag.component";
 import { SearchComponent } from "./search/search.component";
 
+
 export const AdminDashboardComponents = [
   AdminDashboardPasswordComponent,
   NewAccountComponent,
+  NewGroupComponent,
   GroupTagDirective,
   GroupTagComponent,
   SearchComponent
---
+++ src/app/admin/components/dashboard/components/new-group/new-group.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+
+@Component({
+    selector: 'dashboard-new-group',
+    templateUrl: 'new-group.component.html',
+    styleUrls: ['new-group.component.scss']
+})
+export class NewGroupComponent {
+    
+    formData: FormGroup;
+
+    @Input() display: boolean = false;
+
+    constructor() {
+        this.formData = new FormGroup({
+            group: new FormControl('', [])
+        })
+    }
+
+    submitForm() {}
+
+    onCancel(event) {
+        console.log('on cancel');
+    }
+}
