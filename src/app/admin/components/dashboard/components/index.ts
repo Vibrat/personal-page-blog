6596804d660296133a80be50ce3bf83bfb13b86f
DESKTOP-G47CM0M\admin <lam.nguyen@onsolve.com>
2019-09-17 21:20:08 +0700

    Add New Group

--- src/app/admin/components/dashboard/components/index.ts
+++ src/app/admin/components/dashboard/components/index.ts
@@ -1,6 +1,6 @@
 import { AdminDashboardPasswordComponent } from "./password/dashboard-password.component";
 import { NewAccountComponent } from "./new-account/new-account.component";
-import { NewGroupComponent } from "./new-group/new-group.component";
+import { NewGroupComponent, SelectOption } from "./new-group/new-group.component";
 import { GroupTagDirective } from "./group-tag/group-tag.directive";
 import { GroupTagComponent } from "./group-tag/group-tag.component";
 import { SearchComponent } from "./search/search.component";
@@ -10,6 +10,7 @@ export const AdminDashboardComponents = [
   AdminDashboardPasswordComponent,
   NewAccountComponent,
   NewGroupComponent,
+  SelectOption,
   GroupTagDirective,
   GroupTagComponent,
   SearchComponent
--- src/app/admin/components/dashboard/components/new-group/new-group.component.ts
+++ src/app/admin/components/dashboard/components/new-group/new-group.component.ts
@@ -1,11 +1,43 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  Directive,
+  HostListener
+} from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { validateDuplicateGroup } from "./validation";
-import { Observable, of } from "rxjs";
+import { of } from "rxjs";
 
 export interface GroupData {
   name: string;
   permissions: string[];
+}
+
+@Directive({
+  selector: "[nz-checkbox][option]"
+})
+export class SelectOption {
+  isClicked: boolean = false;
+  constructor(public ref: ElementRef) {}
+  click(alwaysChecked = false, state) {
+    console.log('click', this.isClicked, 'state', state);
+    if (alwaysChecked && this.isClicked != state) {
+      return;
+    }
+    this.isClicked = !this.isClicked;
+    this.ref.nativeElement.click(); 
+  }
+
+  @HostListener('click', []) onClick() {
+    console.log('found click');
+    this.isClicked = !this.isClicked;
+  }
 }
 
 @Component({
@@ -14,10 +46,13 @@ export interface GroupData {
   styleUrls: ["new-group.component.scss"]
 })
 export class NewGroupComponent implements OnInit {
+  @ViewChildren(SelectOption) checkBoxes: QueryList<SelectOption>;
+
   data: GroupData;
   formData: FormGroup;
   currentStep = 0;
   currentStepName = "";
+  stateSelection: boolean = false;
 
   @Input() display: boolean = false;
   @Input() permissions: string[] = [];
@@ -27,7 +62,11 @@ export class NewGroupComponent implements OnInit {
 
   ngOnInit() {
     this.formData = new FormGroup({
-      group: new FormControl("", [], [validateDuplicateGroup(this.groupValidation, this.delayDetection)])
+      group: new FormControl(
+        "",
+        [Validators.required, Validators.pattern("[a-zA-Z0-9]*")],
+        [validateDuplicateGroup(this.groupValidation, this.delayDetection)]
+      )
     });
   }
 
@@ -49,6 +88,11 @@ export class NewGroupComponent implements OnInit {
 
   closeModal() {
     this.display = false;
+  }
+
+  selectAll() {
+    this.checkBoxes.forEach((item: SelectOption) => item.click(true, this.stateSelection));
+    this.stateSelection = !this.stateSelection;
   }
 
   pre() {
