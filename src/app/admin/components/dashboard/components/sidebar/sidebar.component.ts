2738b405117c38424eaf521cedcec90f4046eb30
Lam Nguyen <nguyenhoanglam229@gmail.com>
2019-11-11 13:25:38 +0700

    added only level menu tree due to data state inconsistency of nz-submenu

--- src/app/admin/components/dashboard/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/dashboard/components/sidebar/sidebar.component.ts
@@ -1,4 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+export interface MenuData {
+  enable: boolean;
+  data: MenuItem[];
+  options: MenuOptions;
+}
+
+export interface MenuItem {
+  url: string;
+  text: string;
+  icon?: string;
+  data?: MenuItem[];
+}
+
+export interface MenuOptions {
+  enableFileManager: boolean;
+}
+
+export type DefaultMenuData = MenuData | undefined;
+
+export const defaultMenuData: DefaultMenuData = {
+  enable: true,
+  data: [
+    {
+      url: '/dashboard/group',
+      text: 'Group',
+      icon: 'team',
+    }
+  ],
+  options: {
+    enableFileManager: true
+  }
+};
 
 @Component({
   selector: 'sidebar',
@@ -7,4 +40,5 @@ import { Component } from '@angular/core';
 })
 export class SideBarComponent {
   isCollapsed = true;
+  @Input() menus: MenuData = defaultMenuData;
 }
