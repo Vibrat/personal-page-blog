7665d75263fdb386c98e07e54d678f67fd8d288b
Lam Nguyen <nguyenhoanglam229@gmail.com>
2019-11-18 14:31:10 +0700

    Refactor config for application

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
 // server.js
 const express = require('express');
+const http = require('http');
+const https = require('https');
 const app = express();
-
-// If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+const port = process.env.PORT || 8088;
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/na-web'));
+app.get('*', function (req, response) {
+  response.sendFile( __dirname + '/dist/na-web/index.html');
+});
+
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+const httpsServer = https.createServer(app);
+
+httpsServer.listen(port);
