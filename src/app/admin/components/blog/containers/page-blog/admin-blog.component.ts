77bb3f88f7397787d760387bff38462e5ff9ac3b
Lam Nguyen <nguyenhoanglam229@gmail.com>
2019-12-10 15:26:29 +0700

    added demo for table blog

--- src/app/admin/components/blog/containers/page-blog/admin-blog.component.ts
+++ src/app/admin/components/blog/containers/page-blog/admin-blog.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
-import { HttpClient } from '@angular/common/http';
-import { NzMessageService } from 'ng-zorro-antd';
+import { HttpClient } from "@angular/common/http";
+import { NzMessageService } from "ng-zorro-antd";
 
 import { ViewportService } from "~/app/shared/services/viewport.service";
 
-const count = 5;
-const fakeDataUrl = 'https://randomuser.me/api/?results=5&inc=name,gender,email,nat&noinfo';
+interface ItemData {
+  id: number;
+  name: string;
+  age: number;
+  address: string;
+}
 
 @Component({
-    selector: `admin-blog`,
-    templateUrl: 'admin-blog.component.html',
-    styleUrls: ['admin-blog.component.scss']
+  selector: `admin-blog`,
+  templateUrl: "admin-blog.component.html",
+  styleUrls: ["admin-blog.component.scss"]
 })
 export class AdminBlogComponent implements OnInit {
-  search: string;
   lgScreen$: Observable<boolean> = this.viewport.onLgScreenObserver();
-  modalDisplay: boolean;
-  initLoading = true; // bug
-  loadingMore = false;
-  data: any[] = [];
-  list: Array<{ loading: boolean; name: any }> = [];
   style = {
     title: {
       xs: {
@@ -52,33 +50,75 @@ export class AdminBlogComponent implements OnInit {
         offset: 8
       }
     }
+  };
+
+  listOfSelection = [
+    {
+      text: "Select All Row",
+      onSelect: () => {
+        this.checkAll(true);
+      }
+    },
+    {
+      text: "Select Odd Row",
+      onSelect: () => {
+        this.listOfDisplayData.forEach(
+          (data, index) => (this.mapOfCheckedId[data.id] = index % 2 !== 0)
+        );
+        this.refreshStatus();
+      }
+    },
+    {
+      text: "Select Even Row",
+      onSelect: () => {
+        this.listOfDisplayData.forEach(
+          (data, index) => (this.mapOfCheckedId[data.id] = index % 2 === 0)
+        );
+        this.refreshStatus();
+      }
+    }
+  ];
+  isAllDisplayDataChecked = false;
+  isIndeterminate = false;
+  listOfDisplayData: ItemData[] = [];
+  listOfAllData: ItemData[] = [];
+  mapOfCheckedId: { [key: string]: boolean } = {};
+
+  constructor(
+    private http: HttpClient,
+    private msg: NzMessageService,
+    public viewport: ViewportService
+  ) {}
+
+  currentPageDataChange($event: ItemData[]): void {
+    this.listOfDisplayData = $event;
+    this.refreshStatus();
   }
 
-  constructor(private http: HttpClient, private msg: NzMessageService, public viewport: ViewportService) {}
+  refreshStatus(): void {
+    this.isAllDisplayDataChecked = this.listOfDisplayData.every(
+      item => this.mapOfCheckedId[item.id]
+    );
+    this.isIndeterminate =
+      this.listOfDisplayData.some(item => this.mapOfCheckedId[item.id]) &&
+      !this.isAllDisplayDataChecked;
+  }
+
+  checkAll(value: boolean): void {
+    this.listOfDisplayData.forEach(
+      item => (this.mapOfCheckedId[item.id] = value)
+    );
+    this.refreshStatus();
+  }
 
   ngOnInit(): void {
-    this.getData((res: any) => {
-      this.data = res.results;
-      this.list = res.results;
-      this.initLoading = false;
-    });
-  }
-
-  getData(callback: (res: any) => void): void {
-    this.http.get(fakeDataUrl).subscribe((res: any) => callback(res));
-  }
-
-  onLoadMore(): void {
-    this.loadingMore = true;
-    this.list = this.data.concat([...Array(count)].fill({}).map(() => ({ loading: true, name: {} })));
-    this.http.get(fakeDataUrl).subscribe((res: any) => {
-      this.data = this.data.concat(res.results);
-      this.list = [...this.data];
-      this.loadingMore = false;
-    });
-  }
-
-  edit(item: any): void {
-    this.msg.success(item.email);
+    for (let i = 0; i < 100; i++) {
+      this.listOfAllData.push({
+        id: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`
+      });
+    }
   }
 }
