5a4b772c289fa2e363615d3d62c71de889f4a958
Lam Nguyen <nguyenhoanglam229@gmail.com>
2019-12-11 16:54:02 +0700

    refactor code for easy understand

--- src/app/admin/components/blog/components/editor/editor.component.ts
+++ src/app/admin/components/blog/components/editor/editor.component.ts
@@ -1,20 +1,37 @@
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import SimpleMDE from "simplemde";
 
+export type Markdown = string;
+
+/**
+ * Editor Component that represnet markdown editor
+ *
+ * @see https://github.com/markedjs/marked
+ * @see https://github.com/sparksuite/simplemde-markdown-editor
+ */
 @Component({
   selector: "md-editor",
   templateUrl: `editor.component.html`,
   styleUrls: ["editor.component.scss"]
 })
 export class EditorComponent implements OnInit {
+
   @ViewChild("editor", { static: true }) editor: ElementRef;
-  md: SimpleMDE;
+  mdEditor: SimpleMDE;
+  content: Markdown;
 
   ngOnInit() {
-    this.md = new SimpleMDE({
+    // Init simplemd
+    this.mdEditor = new SimpleMDE({
       element: this.editor.nativeElement,
       forceSync: true,
       status: false
     });
+
+    // listen on change
+    this.mdEditor.codemirror.on(
+      "change",
+      () => (this.content = this.mdEditor.value())
+    );
   }
 }
